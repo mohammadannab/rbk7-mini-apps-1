1861705e59ada05d432770b63f7d889f6709f68a
mohammad annab <eng.mannab@yahoo.com>
2019-10-29 20:23:58 +0200

    challenge 2

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,43 @@
+  $(document).ready(function () {
+
+        var items = [
+              { name: "Item 1", color: "Green", size: "X-Large" },
+              { name: "Item 2", color: "Green", size: "X-Large" },
+              { name: "Item 3", color: "Green", size: "X-Large" }];
+
+              var keys = [];
+              for(var i = 0; i < items.length; i++){
+                for (var key in obj){
+                    keys.push(key);
+                }
+              }
+              
+
+        var jsonObject = JSON.stringify(items);
+
+        $('').text(jsonObject);
+
+        $('').text(ConvertToCSV(jsonObject));
+    });
+
+
+
+ function ConvertToCSV(objArray) {
+            var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+            var str = '';
+
+            for (var i = 0; i < array.length; i++) {
+                var line = '';
+                for (var index in array[i]) {
+                    if (line != '') line += ','
+
+                    line += array[i][index];
+                }
+
+                str += line + '\r\n';
+            }
+
+            return str;
+        }
 
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,26 @@
 
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('client'));
 
+
+app.get('/RP', function (req, res) {
+	res.sendfile('index.html');
+});
+
+app.post('/RP', function (req, res) {
+  // res.sendfile('index.html');
+  var change = (req.body.data);
+  console.log (req.body.data);
+  res.end("your data is being converted to CSV");
+});
+
+
+
+
+
+app.listen(3000, function(){
+  console.log("port 3000 is awesome!")
+});
